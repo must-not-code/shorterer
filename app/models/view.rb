31102ab06f3eb4f01eee11a41815e5378d382540
must-not-code <test4food@me.com>
2021-03-28 18:02:23 +0300

    Research with seeds

--- app/models/view.rb
+++ app/models/view.rb
@@ -3,4 +3,36 @@ class View < ApplicationRecord
   include Elasticsearch::Model::Callbacks
 
   belongs_to :short_url, counter_cache: true
+
+  def as_indexed_json(options = {})
+    self.as_json(
+      only: [:id, :platform, :device, :country, :created_at],
+      include: {
+        short_url: {
+          only: [:slug]
+        }
+      }
+    )
+  end
+
+  def self.test_search(slug)
+    View.__elasticsearch__.search(
+      query: {
+        bool: {
+          must: {
+            match: {
+              'short_url.slug': slug
+            }
+          },
+          filter: {
+            range: {
+              created_at: {
+                gte: 'now-30d'
+              }
+            }
+          }
+        }
+      }
+    )
+  end
 end
